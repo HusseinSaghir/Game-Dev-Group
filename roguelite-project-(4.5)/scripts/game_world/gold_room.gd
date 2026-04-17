c886863c4fa4f985018fe726a0ab6de0cec5d5c5
Aiden Barcome <aidenbarcode@gmail.com>
2026-04-17 00:14:52 -0400

    Integrated ProcGen map into main project.

---
+++ roguelite-project-(4.5)/scripts/game_world/gold_room.gd
@@ -0,0 +1,11 @@
+class_name GoldRoom
+extends RoomType
+
+#const GOLD_DATA = preload("res://Data/Rocks/gold.tres")
+
+func get_dimensions() -> Vector2i:
+	var side := randi_range(8, 10)
+	return Vector2i(side, side)
+	
+#func get_ore_data() -> Resource:
+	#return GOLD_DATA
