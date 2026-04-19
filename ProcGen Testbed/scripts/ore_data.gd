9e59a12ac3800347523fdb58176c6164eea17c94
Aiden Barcome <aidenbarcode@gmail.com>
2026-04-19 13:19:16 -0400

    Resolved resolution issue.

--- ProcGen Testbed/scripts/ore_data.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name OreData
-
-@export var name: String
-@export var texture: Texture2D
-@export var value: int
--- ProcGen Testbed/scripts/rock_data.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name RockData
-
-@export var texture: Texture2D
-@export var maxHealth: int
-@export var rarity: int
-@export var minDepth: int
-@export var oreResource: OreData
