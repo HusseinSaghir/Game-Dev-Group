c9a4017b7da1cc9b452c6a780a1102dc667b0b45
Kaleb Tomanovich <kalebtomanovich@gmail.com>
2026-04-22 11:06:31 -0400

    My updated of my project includes the not having passive items show on the in game hud and the code for removing pick up items from the inventory when the player runs into an object like a shop or slot machine

--- roguelite-project/scripts/player/Inventory.gd
+++ roguelite-project/scripts/player/Inventory.gd
@@ -15,4 +15,22 @@ func add_resources(type : ResourceItem, amount : int):
 		resources[type] = resources[type] + amount
 	else:
 		resources[type] = amount
-	emit_signal("resource_count_changed", type, resources[type])
+	#This checks to see if the item that was picked up is a Pickup item or a Passive item for iventory stuff
+	if(type.group == "Pickup"):
+		emit_signal("resource_count_changed", type, resources[type])
+
+# When this function is called it will check the inventory for any pick up and if it has it
+# It will remove the amount from that pick up
+# It will also check to see if the pickup in the inventory is at 0 or not and if it is the function will pass through it
+# Make sure when you call this function from another file to make sure that the string variable matches up perfecting with the resource
+# If there is any space in the string variable it will not work
+func remove_resrouces(type : String, amount : int):
+	for i in resources:
+		if (i.display_name == type):
+			if(resources[i] > 0):
+				resources[i] = resources[i] - amount
+				
+				emit_signal("resource_count_changed", i, resources[i])
+				return "passed"
+	return "no_passed"
+	
